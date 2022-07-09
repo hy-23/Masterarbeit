672f21776b105b55b192bf7fb079d461664a552c
Harsha Yogeshappa <yogeshappa@lfb.rwth-aachen.de>
2022-07-09 17:24:41 +0200

    Added Matlab script.
    1. Matlab script to print macro lines for Fiji to autoscale the images.

---
+++ src/windows/utils/matlab/hThesis_callAutoScaleImages.m
@@ -0,0 +1,26 @@
+%%
+%% Matlab script to load the filenames in the list.txt file and pass it to 
+%% hThesis_AutoScaleImages.m file
+%%
+%% Author: Harsha Yogeshappa
+%%
+
+function hThesis_callAutoScaleImages(width, height, slices, ...
+                                     pixelwidth, pixelheight, voxeldepth)
+fileId = fopen('D:\Harsha\repo\Masterarbeit\dataset\larvalign_data-affine_registered\DataSetGoodQual\list.txt', 'r');
+fileList = {};
+fileCount = 0;
+tline = fgetl(fileId);
+
+while ischar(tline)
+    % https://de.mathworks.com/matlabcentral/answers/87549-append-to-an-array
+    fileList = [fileList, tline];
+    fileCount = fileCount + 1;
+    tline = fgetl(fileId);
+end
+
+for i = 1 : fileCount
+    filename = fileList{i};
+    hThesis_AutoScaleImages(filename, width, height, slices, pixelwidth, pixelheight, voxeldepth);
+end
+end
