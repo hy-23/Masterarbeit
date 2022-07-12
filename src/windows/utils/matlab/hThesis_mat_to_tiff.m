91d789c9202864c8f82a2fe0035d33fb8f066b73
Harsha Yogeshappa <yogeshappa@lfb.rwth-aachen.de>
2022-07-12 15:30:57 +0200

    Add py and mat scripts to convert npz to tif files.
    1. python file would convert npz to mat.
    2. matlab file would convert mat to tif.

---
+++ src/windows/utils/matlab/hThesis_mat_to_tiff.m
@@ -0,0 +1,20 @@
+%%
+%% Matlab script to write a tiff file out the of the matlab variable. 
+%% hThesis_mat_to_tiff.m file
+%%
+%% Author: Harsha Yogeshappa
+%%
+function hThesis_mat_to_tiff(mat_var, filename)
+warning('off','MATLAB:MKDIR:DirectoryExists');
+
+out = 'I:\tensorflow_out\out\matlab';
+mkdir(out);
+% 'mat_var' is output of CNN model and thus the values will be between 0.0
+% and 1.0. Therefore, scaling to 255 and casting to uint8 becomes necessary. 
+a = uint8(mat_var * 255);
+[~, ~, z] = size(a);
+
+for i = 1 : z
+    imwrite(a(:,:,i), [out '\' filename '.tif'], "WriteMode", "append");
+end
+end
